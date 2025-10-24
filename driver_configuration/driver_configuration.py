017172b7a83447d6bc751c897a59fd4c337d46e4
SpiderCoop <david.jimenez.cooper@gmail.com>
2025-10-23 21:26:53 -0600

    Added parameter to select a headless driver or not and other minor changes

--- driver_configuration/driver_configuration.py
+++ driver_configuration/driver_configuration.py
@@ -12,18 +12,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-
-def driver_configuration(download_save_path:str = None):
+def driver_configuration(download_save_path:str = None, headless:bool = True):
     """
-    Selenium's driver configuration with automatic file download options and headless mode to enable background execution and Github Actions compatibility.
+    Selenium's driver configuration with automatic file download options and headless mode
 
     Parameters
     ----------
     download_save_path : str, optional
         Route to save downloaded files. The default is None.
+    headless : bool, optional
+        Enable headless mode for background execution and Github Actions compatibility. The default is True.
+
     Returns
     -------
     driver : selenium.webdriver.Chrome
@@ -34,7 +35,21 @@ def driver_configuration(download_save_path:str = None):
         os.makedirs(download_save_path)
 
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
+
+    if headless:
+        chrome_options.add_argument("--headless=new")
+
+    if download_save_path:
+        if not os.path.exists(download_save_path):
+            os.makedirs(download_save_path)
+
+        chrome_options.add_experimental_option("prefs", {
+            "download.default_directory": download_save_path,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        })
+
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -43,14 +58,6 @@ def driver_configuration(download_save_path:str = None):
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--remote-debugging-port=0")
     chrome_options.add_argument("--log-level=3")
-
-    if download_save_path:
-        chrome_options.add_experimental_option("prefs", {
-            "download.default_directory": download_save_path,
-            "download.prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "safebrowsing.enabled": True
-        })
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
